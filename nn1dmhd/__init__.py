7e1f4774f14726a25db19234562b234ed421229c
Eric Winter <eric.winter@jhuapl.edu>
2022-08-27 13:14:21 -0400

    Added nn1dmhd tree and tox.ini.

---
+++ nn1dmhd/__init__.py
@@ -0,0 +1,9 @@
+"""nn1dmhd - solve 1-D MHD problems with neural networks.
+
+Modules
+-------
+
+Packages
+--------
+tests
+"""
---
+++ nn1dmhd/tests/test_nn1dmhd.py
@@ -0,0 +1,17 @@
+"""Tests for the nn1dmhd package."""
+
+
+import unittest
+
+import nn1dmhd
+
+
+class TestBuilder(unittest.TestCase):
+
+    def test___init__(self):
+        """Test the __init__.py module."""
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
