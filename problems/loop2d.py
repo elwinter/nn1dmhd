5468ee002420a20236e04eb4520cf249ebe9fdf1
Eric Winter <eric.winter62@gmail.com>
2023-01-22 14:55:54 -0500

    Double-checked all equations and constants, removed unused code.

--- problems/loop2d.py
+++ problems/loop2d.py
@@ -1,10 +1,36 @@
 """Problem definition file for a simple 2-D MHD problem.
 
-This problem definition file describes a static situation: unit pressure and
-density, all else is 0. The run should just converge to the initial values for
-each quantity.
+This problem definition file describes the 2-D field loop convection problem
+which is part of the Athena MHD test suite. Details are available at:
 
-NOTE: These equations were verified on 2022-11-18.
+https://www.astro.princeton.edu/~jstone/Athena/tests/field-loop/Field-loop.html
+
+From the description on that web page:
+
+    The test involves advecting a field loop (cylindrical current distribution)
+    diagonally across the grid. Any arbitrary angle can be chosen. For the 2D
+    results shown here, the problem domain is -1 ≤ x ≤ 1;
+    -1/(2*cos(30)) ≤ y ≤ 1/(2*cos(30)), and the flow is inclined at 60 degrees
+    to the y-axis. This geometry ensures the flow does not cross the grid along
+    a diagonal, so the fluxes in x- and y-directions will be different.
+
+    The flow velocity is 1.0, so that Vx=sin(60) and Vy=cos(60). The density
+    and pressure are both 1.0, and the gas constant is γ = 5/3. Periodic
+    boundary conditions are used everywhere.
+
+    The magnetic field is initialized using an arbitrary vector potential
+    defined at zone corners; we use Az = MAX([A ( R0 - r )],0). The amplitude
+    A must be small so that the field is weak compared to the gas pressure. A
+    stronger field would require a more careful choice of Az so that the loop
+    is in magnetostatic equilibrium). We use A = 1.0e-3 and a radius for the
+    loop R0 = 0.3. Face-centered magnetic fields are computed using
+    B = ∇ ⊗ Az to guarantee ∇ ⋅ B = 0 initially. Note for the vector potential
+    we have adopted, the second derivative (current density) is discontinuous.
+    There is a line current at the center of the loop, and a surface return
+    current. These currents are neither resolved nor smooth (see the images
+    and movie below) -- if anything this makes the test harder. A different
+    vector potential which gives smooth currents could be adopted for
+    convergence tests.
 
 NOTE: The functions in this module are defined using a combination of Numpy and
 TensorFlow operations, so they can be used efficiently by the TensorFlow
@@ -20,14 +46,16 @@ independent variables:
 NOTE: In all code, below, the following indices are assigned to physical
 dependent variables:
 
-    0: n
-    1: P
-    2: ux
-    3: uy
-    4: uz
-    5: Bx
-    6: By
-    7: Bz
+    0: n (number density)
+    1: P (pressure)
+    2: ux (x-component of fluid velocity)
+    3: uy (y-component of fluid velocity)
+    4: uz (z-component of fluid velocity)
+    5: Bx (x-component of magnetic field)
+    6: By (y-component of magnetic field)
+    7: Bz (z-component of magnetic field)
+
+NOTE: These equations were last verified on 2023-01-22.
 
 Author
 ------
@@ -38,18 +66,16 @@ Eric Winter (eric.winter62@gmail.com)
 # Import standard modules.
 
 # Import supplemental modules.
-import numpy as np
 import tensorflow as tf
 
 # Import project modules.
-from nn1dmhd.training_data import create_training_points_gridded
 
 
 # Names of independent variables.
 independent_variable_names = ['t', 'x', 'y']
 
 # Labels for independent variables (may use LaTex) - use for plots.
-independent_variable_labels = ['$t$', '$x$', '$y$']
+independent_variable_labels = ["$t$", "$x$", "$y$"]
 
 # Number of problem dimensions (independent variables).
 n_dim = len(independent_variable_names)
@@ -59,211 +85,19 @@ dependent_variable_names = ['n', 'P', 'ux', 'uy', 'uz', 'Bx', 'By', 'Bz']
 
 # Labels for dependent variables (may use LaTex) - use for plots.
 dependent_variable_labels = [
-    '$n$', '$P$',
-    '$u_x$', '$u_y$', '$u_z$',
-    '$B_x$', '$B_y$', '$B_z$'
+    "$n$", "$P$",
+    "$u_x$", "$u_y$", "$u_z$",
+    "$B_x$", "$B_y$", "$B_z$"
 ]
 
 # Number of dependent variables.
 n_var = len(dependent_variable_names)
 
 
-# Define the problem domain.
-t0 = 0.0
-t1 = 1.0
-x0 = 0.0
-x1 = 1.0
-y0 = 0.0
-y1 = 1.0
-domain = tf.reshape(
-    [[t0, t1],
-     [x0, x1],
-     [y0, y1]],
-    (n_dim, 2)
-)
-
-# Adiabatic index = (N + 2)/N, N = # DOF.
-ɣ = 2
-
 # Normalized physical constants.
 μ0 = 1.0  # Permeability of free space
-me = 1.0  # Electron mass
-
-# Initial values for each dependent variable (dimensionless).
-n0 = 1.0
-P0 = 1.0
-ux0 = 0.0
-uy0 = 0.0
-uz0 = 0.0
-Bx0 = 0.0
-By0 = 0.0
-Bz0 = 0.0
-initial_values = [n0, P0, ux0, uy0, uz0, Bx0, By0, Bz0]
-
-
-def n_analytical(X: np.ndarray):
-    """Compute analytical solution for number density.
-
-    Compute anaytical solution for number density.
-
-    Parameters
-    ----------
-    X : np.ndarray of float, shape (n, n_dim)
-        Independent variable values for computation.
-
-    Returns
-    -------
-    n : np.ndarray of float, shape (n,)
-        Analytical values for number density.
-    """
-    n = np.full((X.shape[0],), n0)
-    return n
-
-
-def P_analytical(X: np.ndarray):
-    """Compute analytical solution for pressure.
-
-    Compute anaytical solution for pressure.
-
-    Parameters
-    ----------
-    X : np.ndarray of float, shape (n, n_dim)
-        Independent variable values for computation.
-
-    Returns
-    -------
-    P : np.ndarray of float, shape (n,)
-        Analytical values for pressure.
-    """
-    P = np.full((X.shape[0],), P0)
-    return P
-
-
-def ux_analytical(X: np.ndarray):
-    """Compute analytical solution for x-velocity.
-
-    Compute anaytical solution for x-velocity.
-
-    Parameters
-    ----------
-    X : np.ndarray of float, shape (n, n_dim)
-        Independent variable values for computation.
-
-    Returns
-    -------
-    ux : np.ndarray of float, shape (n,)
-        Analytical values for x-velocity.
-    """
-    ux = np.full((X.shape[0],), ux0)
-    return ux
-
-
-def uy_analytical(X: np.ndarray):
-    """Compute analytical solution for y-velocity.
-
-    Compute anaytical solution for y-velocity.
-
-    Parameters
-    ----------
-    X : np.ndarray of float, shape (n, n_dim)
-        Independent variable values for computation.
-
-    Returns
-    -------
-    uy : np.ndarray of float, shape (n,)
-        Analytical values for y-velocity.
-    """
-    uy = np.full((X.shape[0],), uy0)
-    return uy
-
-
-def uz_analytical(X: np.ndarray):
-    """Compute analytical solution for z-velocity.
-
-    Compute anaytical solution for z-velocity.
-
-    Parameters
-    ----------
-    X : np.ndarray of float, shape (n, n_dim)
-        Independent variable values for computation.
-
-    Returns
-    -------
-    uz : np.ndarray of float, shape (n,)
-        Analytical values for z-velocity.
-    """
-    uz = np.full((X.shape[0],), uz0)
-    return uz
-
-
-def Bx_analytical(X: np.ndarray):
-    """Compute analytical solution for x-magnetic field.
-
-    Compute anaytical solution for x-magnetic field.
-
-    Parameters
-    ----------
-    X : np.ndarray of float, shape (n, n_dim)
-        Independent variable values for computation.
-
-    Returns
-    -------
-    Bx : np.ndarray of float, shape (n,)
-        Analytical values for x-magnetic field.
-    """
-    Bx = np.full((X.shape[0],), Bx0)
-    return Bx
-
-
-def By_analytical(X: np.ndarray):
-    """Compute analytical solution for y-magnetic field.
-
-    Compute anaytical solution for y-magnetic field.
-
-    Parameters
-    ----------
-    X : np.ndarray of float, shape (n, n_dim)
-        Independent variable values for computation.
-
-    Returns
-    -------
-    By : np.ndarray of float, shape (n,)
-        Analytical values for y-magnetic field.
-    """
-    By = np.full((X.shape[0],), By0)
-    return By
-
-
-def Bz_analytical(X: np.ndarray):
-    """Compute analytical solution for z-magnetic field.
-
-    Compute anaytical solution for z-magnetic field.
-
-    Parameters
-    ----------
-    X : np.ndarray of float, shape (n, n_dim)
-        Independent variable values for computation.
-
-    Returns
-    -------
-    Bz : np.ndarray of float, shape (n,)
-        Analytical values for z-magnetic field.
-    """
-    Bz = np.full((X.shape[0],), Bz0)
-    return Bz
-
-
-# Gather all of the analytical solutions into a list.
-Y_analytical = [
-    n_analytical,
-    P_analytical,
-    ux_analytical,
-    uy_analytical,
-    uz_analytical,
-    Bx_analytical,
-    By_analytical,
-    Bz_analytical,
-]
+m = 1.0   # Particle mass
+ɣ = 5/3   # Adiabatic index = (N + 2)/N, N = # DOF=3, not 2.
 
 
 # @tf.function
@@ -280,8 +114,8 @@ def pde_n(X, Y, del_Y):
     Y : list of n_var tf.Tensor, each shape (n, 1)
         Values of dependent variables at each evaluation point.
     del_Y : list of n_var tf.Tensor, each shape (n, n_dim)
-        Values of gradients of dependent variables wrt independent variables at
-        each evaluation point.
+        Values of gradients of dependent variables wrt independent variables
+        at each evaluation point.
 
     Returns
     -------
@@ -338,8 +172,8 @@ def pde_P(X, Y, del_Y):
     Y : list of n_var tf.Tensor, each shape (n, 1)
         Values of dependent variables at each evaluation point.
     del_Y : list of n_var tf.Tensor, each shape (n, n_dim)
-        Values of gradients of dependent variables wrt independent variables at
-        each evaluation point.
+        Values of gradients of dependent variables wrt independent variables
+        at each evaluation point.
 
     Returns
     -------
@@ -378,7 +212,7 @@ def pde_P(X, Y, del_Y):
     # dBz_dy = tf.reshape(del_Bz[:, 2], (nX, 1))
 
     # G is a Tensor of shape (n, 1).
-    G = -ɣ*P/n*(dn_dt + ux*dn_dx + uy*dn_dy) + (dP_dt + ux*dP_dx + uy*dP_dy)/me
+    G = -ɣ*P/n*(dn_dt + ux*dn_dx + uy*dn_dy) + (dP_dt + ux*dP_dx + uy*dP_dy)/m
     return G
 
 
@@ -437,8 +271,8 @@ def pde_ux(X, Y, del_Y):
 
     # G is a Tensor of shape (n, 1).
     G = (
-        n*(dux_dt + ux*dux_dx + uy*dux_dy) + dP_dx/me +
-        (By*(dBy_dx - dBx_dy) + Bz*dBz_dx)/(me*μ0)
+        n*(dux_dt + ux*dux_dx + uy*dux_dy) + dP_dx/m +
+        (By*(dBy_dx - dBx_dy) + Bz*dBz_dx)/(m*μ0)
     )
     return G
 
@@ -498,8 +332,8 @@ def pde_uy(X, Y, del_Y):
 
     # G is a Tensor of shape (n, 1).
     G = (
-        n*(duy_dt + ux*duy_dx + uy*duy_dy) + dP_dy/me +
-        (Bx*(dBx_dy - dBy_dx) + Bz*dBz_dy)/(me*μ0)
+        n*(duy_dt + ux*duy_dx + uy*duy_dy) + dP_dy/m +
+        (Bx*(dBx_dy - dBy_dx) + Bz*dBz_dy)/(m*μ0)
     )
     return G
 
@@ -558,7 +392,7 @@ def pde_uz(X, Y, del_Y):
     dBz_dy = tf.reshape(del_Bz[:, 2], (nX, 1))
 
     # G is a Tensor of shape (n, 1).
-    G = n*(duz_dt + ux*duz_dx + uy*duz_dy) - (Bx*dBz_dx + By*dBz_dy)/(me*μ0)
+    G = n*(duz_dt + ux*duz_dx + uy*duz_dy) - (Bx*dBz_dx + By*dBz_dy)/(m*μ0)
     return G
 
 
@@ -760,27 +594,6 @@ if __name__ == "__main__":
     print("dependent_variable_labels = %s" % dependent_variable_labels)
     print("n_var = %s" % n_var)
 
-    print("%s <= t <= %s" % (t0, t1))
-    print("%s <= x <= %s" % (x0, x1))
-    print("%s <= y <= %s" % (y0, y1))
-    print("domain = %s" % domain)
-
-    print("n0 = %s" % n0)
-    print("P0 = %s" % P0)
-    print("ux0 = %s" % ux0)
-    print("uy0 = %s" % uy0)
-    print("uz0 = %s" % uz0)
-    print("Bx0 = %s" % Bx0)
-    print("By0 = %s" % By0)
-    print("Bz0 = %s" % Bz0)
-
-    nt = 3
-    nx = 4
-    ny = 5
-    Xg, Xg_in, Xg_bc = create_training_data_gridded(nt, nx, ny)
-    print("Xg = %s" % Xg)
-    print("Xg_in = %s" % Xg_in)
-    print("Xg_bc = %s" % Xg_bc)
-
-    bc = compute_boundary_conditions(Xg_bc)
-    print("bc = %s" % bc)
+    print("μ0 = %s" % μ0)
+    print("m = %s" % m)
+    print("ɣ = %s" % ɣ)
