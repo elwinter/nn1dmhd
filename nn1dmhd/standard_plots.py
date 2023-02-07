2a818930683f316f972a374d20b1665756fe45a9
Eric Winter <eric.winter@jhuapl.edu>
2023-02-07 18:05:09 -0500

    Added plot_BxBy_quiver and plot_logarithmic_heatmap().

--- nn1dmhd/standard_plots.py
+++ nn1dmhd/standard_plots.py
@@ -12,8 +12,10 @@ Eric Winter (eric.winter62@gmail.com)
 # Import standard modules.
 
 # Import supplemental modules.
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 
 # Import project modules.
 
@@ -43,4 +45,63 @@ def plot_loss_functions(L_res, L_data, L, ax, title="Loss functions"):
     ax.set_ylabel("Loss function")
     ax.grid()
     ax.legend()
+    ax.set_title(title)
+
+
+def plot_BxBy_quiver(x, y, Bx, By, ax, title="Magnetic field"):
+    """Plot Bx and By components as a quiver plot.
+
+    Plot Bx and By components as a quiver plot.
+
+    Parameters
+    ----------
+    x, y : np.ndarray, shape ( n,)
+        x and y coordinates for arrows.
+    Bx, By : np.ndarray, shape ( n,)
+        x- and y-components for arrows.
+    ax : matplotlib.Axes
+        Axes object to use for the plot.
+    title : str (optional, default "Magnetic field")
+        Title for plot
+
+    Returns
+    -------
+    None
+    """
+    ax.quiver(x, y, Bx, By)
+    ax.set_aspect(1.0)
+    ax.set_xlabel("$x$")
+    # ax.set_xticks(XY_x_tick_pos, XY_x_tick_labels)
+    ax.set_ylabel("$y$")
+    # ax.set_yticks(XY_y_tick_pos, XY_y_tick_labels)
+    ax.grid()
+    ax.set_title(title)
+
+
+def plot_logarithmic_heatmap(z, ax, title="Heat map"):
+    """Plot Bx and By components as a quiver plot.
+
+    Plot Bx and By components as a quiver plot.
+
+    Parameters
+    ----------
+    x, y : np.ndarray, shape ( n,)
+        x and y coordinates for arrows.
+    Bx, By : np.ndarray, shape ( n,)
+        x- and y-components for arrows.
+    ax : matplotlib.Axes
+        Axes object to use for the plot.
+    title : str (optional, default "Magnetic field")
+        Title for plot
+
+    Returns
+    -------
+    # None
+    """
+    sns.heatmap(z, ax=ax, norm=mpl.colors.LogNorm(), square=True)
+    ax.set_xlabel("$x$")
+    # ax.set_xticks(heatmap_x_tick_pos, heatmap_x_tick_labels, rotation=0)
+    ax.set_ylabel("$y$")
+    # ax.set_yticks(heatmap_y_tick_pos, heatmap_y_tick_labels)
+    ax.grid()
     ax.set_title(title)
